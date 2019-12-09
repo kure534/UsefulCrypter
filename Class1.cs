60dd4dd219d7a7817848a7274ef86f366e77730c
Taras <taras.harbar@gmail.com>
2019-12-09 16:38:51 +0300

    initial

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,55 @@
 ﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+
 
 namespace UsefulCryptor
 {
-    public class Class1
+    public interface ICryptor<T>
     {
+        IMessage<T> Crypt(IMessage<T> message);
     }
+    public interface IEncryptor<T>
+    {
+        IMessage<T> Encrypt(IMessage<T> message);
+    }
+    public interface IMessage<T>
+    {
+        Dictionary<FieldInfo, T> GetElements();
+    }
+    public class TextMessage<T> : IMessage<T>
+    {
+        Dictionary<FieldInfo, T> IMessage<T>.GetElements()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class SerializedMessage<T>
+    {
+        public Type messageType;
+        public Dictionary<FieldInfo, T> elementsValue = new Dictionary<FieldInfo, T>();
+        private readonly int hash;
+
+        public bool IsCrypted()
+        {
+            return this.GetHashCode().Equals(hash);
+        }
+        public SerializedMessage(IMessage<T> message)
+        {
+            messageType = message.GetType();
+            elementsValue = message.GetElements();
+            hash = GetHashCode();
+        }
+        public object Deserialize()
+        {
+            object obj = messageType.GetConstructor(null).Invoke(null);
+            foreach (var item in elementsValue)
+            {
+                item.Key.SetValue(obj, item.Value);
+            }
+            return obj;
+        }
+    }
+
 }
